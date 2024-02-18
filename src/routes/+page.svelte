c314a02ed8908b2ed66e24a8deb387921113dafa
Joel Ethan Batac <bjoelethan@gmail.com>
2024-02-19 03:49:39 +0800

    started with reusable Article component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+
+    import Article from '$lib/components/Article.svelte'
     
     let count = 0;
     $: double = count * 2;
@@ -13,22 +15,58 @@
        
     }, 2000)
 
-</script>
+</script> 
 
 <div class="wrapper main">
-    dito pala dapat nilalagay
+    <div class="left">
+        dito pala dapat nilalagay
+    </div>
+    <div class="middle">
+        <Article>
+        </Article>
+
+        <hr>
+
+        <Article>
+        </Article>
+    </div>
+    <div class="right">
+        Test
+    </div>
 </div>
 
+
 <style>
-    .main {
-        display: grid;
-        grid-template-columns: 1fr;
-    }
+
+    @media (min-width: 475px){
+        .main {
+            display: grid;
+            grid-template-columns: 1fr;
+        }
+
+        .left, .right{
+            display: none;
+        }
+
+        .middle{
+            display: block;
+        }
+    }   
 
     @media (min-width: 768px) {
         .main {
             grid-template-columns: 3fr 1fr;
             grid-template-areas: "middle right";
+        }
+
+        .left{
+            display: none;
+        }
+
+        .middle, .right{
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
 
     }
@@ -37,6 +75,10 @@
         .main {
             grid-template-columns: 1fr 3fr 1fr;
             grid-template-areas: "left middle right";
+        }
+
+        .left, .middle, .right{
+            display: block;
         }
         
     }

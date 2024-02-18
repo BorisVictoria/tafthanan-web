d82775eaed2f0099f87d6113e32c0d8f79370652
Boris Victoria <boris.v.victoria@gmail.com>
2024-02-18 20:09:50 +0800

    Add CSS reset, responsive containers, and font families

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+    import '../reset.css'
+    import '../app.css'
+</script>
+
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+</svelte:head>
+
+<div class="wrapper">
+    <header>
+        
+    </header>
+</div>
+
+
+<slot>
+
+</slot>
+<style>
+    
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    
+    let count = 0;
+    $: double = count * 2;
+
+    function increment() {
+        count++;
+        
+    }
+
+    setInterval(() => {
+        count++;
+       
+    }, 2000)
+
+</script>
+<div class="wrapper">
+    <p>The number of times I've had it is {count}</p>
+</div>
+<style>
+    button {
+        width: 5rem;
+        height: 5rem;
+
+    }
+</style>

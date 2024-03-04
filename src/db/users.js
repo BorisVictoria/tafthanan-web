3fdb1bef817ca3d1dd5db450465dcfbd48ed9d83
Boris Victoria <boris.v.victoria@gmail.com>
2024-03-05 01:00:51 +0800

    Add hashing and token authentication to login

--- src/db/users.js
+++ src/db/users.js
@@ -1,4 +1,5 @@
 import client from '$db/mongo'
+import {ObjectId} from 'mongodb'
 
 export const users = client.db('tafthanan').collection('users')
 
@@ -14,4 +15,26 @@ export const loginUser = async(data) => {
     const result = await users.findOne({username: data.username, password: data.password})
     return result
 
+}
+
+export const getUser = async(username) => {
+
+    const result = await users.findOne({username: username})
+    return result
+}
+
+export const getToken = async(token) => {
+    
+    const result = await users.findOne({token: token})
+    if (result.tokenExpiry < Date.now()) {
+        return null
+    }
+
+    return result
+}
+
+export const setToken = async(data) => {
+    
+    const result = await users.updateOne({username: data.username}, {$set: {token: data.token, tokenExpiry: data.tokenExpiry}})
+    return result
 }

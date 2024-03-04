34587436d6ec8df96e05b218f02beaa809964028
Boris Victoria <boris.v.victoria@gmail.com>
2024-03-05 01:05:09 +0800

    Add route to user profile

---
+++ src/routes/(home)/u/[username]/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import {EJSON} from 'bson'
+    export let data;
+    data = EJSON.deserialize(data)
+    const {user} = data
+
+</script>
+
+<h1>User Information</h1>
+<p>UserID: {user._id}</p>
+<p>Username: {user.username}</p>
+<p>Email: {user.email}</p>
+<p>Password: {user.password}</p>
+
+
+<style>
+
+</style>

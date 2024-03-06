5a6d01dce33cf0adb9e1aadc951d65c044557cfb
Boris Victoria <boris.v.victoria@gmail.com>
2024-03-06 18:42:02 +0800

    Add get user by object id

--- src/db/users.js
+++ src/db/users.js
@@ -1,40 +1,58 @@
 import client from '$db/mongo'
-import {ObjectId} from 'mongodb'
+import { ObjectId } from 'mongodb'
 
 export const users = client.db('tafthanan').collection('users')
 
 export const registerUser = async(data) => {
 
     const result = await users.insertOne(data)
-    return result
-
-}
-
-export const loginUser = async(data) => {
-
-    const result = await users.findOne({username: data.username, password: data.password})
-    return result
+    if (result) {
+        return result
+    }
+    
+    return null
 
 }
 
 export const getUser = async(username) => {
 
     const result = await users.findOne({username: username})
-    return result
+    if (result) {
+        return result
+    }
+
+    return null
+}
+
+export const getUserByID = async(id) => {
+
+    const objID = new ObjectId(id)
+    const result = await users.findOne({_id: objID})
+    if (result) {
+        return result
+    }
+
+    return null
 }
 
 export const getToken = async(token) => {
     
     const result = await users.findOne({token: token})
-    if (result.tokenExpiry < Date.now()) {
-        return null
+    if (result.tokenExpiry > Date.now()) {
+        return result
     }
 
-    return result
+    return null
 }
 
 export const setToken = async(data) => {
     
     const result = await users.updateOne({username: data.username}, {$set: {token: data.token, tokenExpiry: data.tokenExpiry}})
-    return result
+    if (result) {
+        return result
+    }
+
+    return null
+    
+
 }

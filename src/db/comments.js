c9f1bd1c20a455ceac1b51920edd124aa7c01072
BorisVictoria <boris_victoria@dlsu.edu.ph>
2024-03-08 09:54:04 +0800

    Complete crud for comments

--- src/db/comments.js
+++ src/db/comments.js
@@ -41,3 +41,49 @@ export const getComment = async(id) => {
     return null
 
 }
+
+export const createComment = async(data) => {
+    
+    let result
+
+    if (data.parentComment === null) {
+        const kwartoID = new ObjectId(data.kwartoID)
+        const postID = new ObjectId(data.postID)
+        result = await comments.insertOne({kwartoID: kwartoID, postID: postID, content: data.content, voteCount: 0, isEdited: false})
+    } else {
+        const kwartoID = new ObjectId(data.kwartoID)
+        const postID = new ObjectId(data.postID)
+        const parentComment = new ObjectId(data.parentComment)
+        result = await comments.insertOne({kwartoID: data.kwartoID, postID: data.postID, parentComment: data.parentComment, content: data.content, voteCount: 0, isEdited: false })
+    }
+
+    if (result) {
+        return result
+    }
+
+    return null
+
+}
+
+export const editComment = async(data) => {
+    const objID = new ObjectId(data._id)
+    const result = await comments.updateOne({_id: objID}, {$set: {content: data.content}})
+    if (result) {
+        return result
+    }
+
+    return null
+}
+
+export const upvoteComment = async(data) => {
+    const objID = new ObjectId(data._id)
+}
+
+export const downvoteComment = async(data) => {
+    const objID = new ObjectId(data._id)
+}
+
+export const deleteComment = async(id) => {
+    const objID = new ObjectId(id)
+    const result = await comments.findOne({_id: objID})
+}

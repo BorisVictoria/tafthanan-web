8b5bef967b5a27611d76958cf9105c344770a77a
BorisVictoria <boris_victoria@dlsu.edu.ph>
2024-03-08 19:14:55 +0800

    Add CRUD and upvoting to posts and comments

--- src/db/posts.js
+++ src/db/posts.js
@@ -3,13 +3,6 @@ import { ObjectId } from 'mongodb'
 
 export const posts = client.db('tafthanan').collection('posts')
 
-export const submitPost = async(data) => {
-
-
-
-}
-
-
 export const getPost = async(id) => {
 
     const objID = new ObjectId(id)
@@ -20,4 +13,99 @@ export const getPost = async(id) => {
 
     return null
 
+}
+
+export const createPost = async(data) => {
+    
+    const result = await posts.insertOne({title: data.title, content: data.content, tags: null, username: data.username, kwarto: data.kwarto, voteCount: 0, datePosted: Date.now()}) 
+
+    if (result) {
+        return result
+    }
+
+    return null
+
+}
+
+export const editPost = async(data) => {
+    const objID = new ObjectId(data._id)
+    const result = await posts.updateOne({_id: objID}, {$set: {title: data.title, content: data.content}})
+    if (result) {
+        return result
+    }
+
+    return null
+}
+
+export const upvotePost = async(data) => {
+    const objID = new ObjectId(data.postID)
+    let result = await getPostVote(data)
+    if (result) {
+        if (result.votes === true) {
+            result = await posts.updateOne({_id: objID}, {$inc: {votes: -1}})
+            result = await updatePostVote(data)
+        }
+        else if (result.votes === false) {
+            result = await posts.updateOne({_id: objID}, {$inc: {votes: 2}})
+            result = await updatePostVote(data)
+        }
+        else {
+            result = await posts.updateOne({_id: objID}, {$inc: {votes: 1}})
+            result = await updatePostVote(data)
+        }
+
+    } else {
+        result = await createPostVote(data)
+        if (result) {
+            result = await posts.updateOne({_id: objID}, {$inc: {votes: 1}})
+            result = await createPostVote(data)
+        } else {
+            return null
+        }
+    }
+
+    if (result) {
+        return result
+    }
+
+    return null
+
+
+
+}
+
+export const downvotePost = async(data) => {
+    const objID = new ObjectId(data.postID)
+    let result = await getPostVote(data)
+    if (result) {
+        if (result.votes === true) {
+            result = await posts.updateOne({_id: objID}, {$inc: {vote: -2}})
+            result = await updatePostVote(data)
+        } else if (result.votes === false) {
+            result = await posts.updateOne({_id: objID}, {$inc: {vote: 1}})
+            result = await updatePostVote(data)
+        } else {
+            result = await posts.updateOne({_id: objID}, {$inc: {vote: -1}})
+            result = await updatePostVote(data)
+        }
+    } else {
+        result = await createPostVote(data)
+        if (result) {
+            result = await posts.update({_id: objID}, {$inc: {votes: -1}})
+            result = await createPostVote(data)
+        } else {
+            return null
+        }
+    }
+
+    if (result) {
+        return result
+    }
+
+    return null
+}
+
+export const deletePost = async(id) => {
+    const objID = new ObjectId(id)
+    const result = await posts.updateOne({_id: objID}, {$set: {deleted: true}})
 }
